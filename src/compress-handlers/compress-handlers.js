674836b1cbfe487bd90df013cb5ac032f2eab5a2
Anton Halauko <anton.halauko.main@gmail.com>
2024-02-04 19:43:02 +0200

    feat: implement decompression task

--- src/compress-handlers/compress-handlers.js
+++ src/compress-handlers/compress-handlers.js
@@ -1,5 +1,7 @@
 import { compressFileHandler } from './compress-file-handler.js';
+import { decompressFileHandler } from './decompress-file-handler.js';
 
 export const compressHandlers = [
-	compressFileHandler
+	compressFileHandler,
+	decompressFileHandler,
 ];
--- src/compress-handlers/decompress-file-handler.js
+++ src/compress-handlers/decompress-file-handler.js
@@ -5,26 +5,26 @@ import * as  utils from 'node:util';
 import * as  stream from 'node:stream';
 import * as  zlib from 'node:zlib';
 
-const COMPRESS_FILE_HANDLER_COMMAND_REGEX = /^compress\s+(\S+)\s+(\S+)/;
+const DECOMPRESS_FILE_HANDLER_COMMAND_REGEX = /^decompress\s+(\S+)\s+(\S+)/;
 
 const pipe = utils.promisify(stream.pipeline);
 
-export const compressFileHandler = async (command, fileManagerState) => {
-	if (!COMPRESS_FILE_HANDLER_COMMAND_REGEX.test(command)) {
+export const decompressFileHandler = async (command, fileManagerState) => {
+	if (!DECOMPRESS_FILE_HANDLER_COMMAND_REGEX.test(command)) {
 		return { isAppropriateHandler: false };
 	}
 
 	const [, fileName, destinationPath] =
-		command.match(COMPRESS_FILE_HANDLER_COMMAND_REGEX);
+		command.match(DECOMPRESS_FILE_HANDLER_COMMAND_REGEX);
 
 	const filePath = path.join(fileManagerState.currentDirectory, fileName);
 	const normalizedPath = path.normalize(destinationPath);
 
-	const compressedFileName = `${fileName}.compressed`;
+	const decompressedFileName = `${fileName}.decompressed`;
 
 	const pathToProcess = path.isAbsolute(normalizedPath)
-		? path.join(normalizedPath, compressedFileName)
-		: path.resolve(fileManagerState.currentDirectory, normalizedPath, compressedFileName);
+		? path.join(normalizedPath, decompressedFileName)
+		: path.resolve(fileManagerState.currentDirectory, normalizedPath, decompressedFileName);
 
 	const userHomeDirectory = os.homedir();
 
@@ -35,7 +35,7 @@ export const compressFileHandler = async (command, fileManagerState) => {
 		};
 	}
 
-	const brotlisCompress = zlib.createBrotliCompress();
+	const brotlisDecompress = zlib.createBrotliDecompress();
 
 	try {
 		const fileHandler = await fs.open(filePath, 'r');
@@ -44,7 +44,7 @@ export const compressFileHandler = async (command, fileManagerState) => {
 		const readStream = fileHandler.createReadStream();
 		const writeStream = destFileHandler.createWriteStream();
 
-		await pipe(readStream, brotlisCompress, writeStream);
+		await pipe(readStream, brotlisDecompress, writeStream);
 
 		return { isAppropriateHandler: true };
 	} catch {
