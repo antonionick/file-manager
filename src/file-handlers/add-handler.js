bfa75291fe2cfc8d0d3727c3f11ffb09b6210151
Anton Halauko <anton.halauko.main@gmail.com>
2024-02-04 12:15:44 +0200

    feat: implement add file task

---
+++ src/file-handlers/add-handler.js
@@ -0,0 +1,26 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+
+const ADD_HANDLER_COMMAND_REGEX = /^add\s+(\S+)/;
+
+export const addHandler = async (command, fileMangerState) => {
+	if (!ADD_HANDLER_COMMAND_REGEX.test(command)) {
+		return { isAppropriateHandler: false };
+	}
+
+	const [, fileName] = command.match(ADD_HANDLER_COMMAND_REGEX);
+
+	try {
+		const filePath = path.join(fileMangerState.currentDirectory, fileName);
+
+		const fileHandler = await fs.open(filePath, 'a');
+		await fileHandler.close();
+
+		return { isAppropriateHandler: true };
+	} catch {
+		return {
+			isAppropriateHandler: true,
+			isOperationFailed: true
+		};
+	}
+};
--- src/file-handlers/file-handlers.js
+++ src/file-handlers/file-handlers.js
@@ -1,5 +1,7 @@
 import { catHandler } from './cat-handler.js';
+import { addHandler } from './add-handler.js';
 
 export const fileHandlers = [
 	catHandler,
+	addHandler,
 ];
