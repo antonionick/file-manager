173e2b397d30130e254865305c9f957b905259c7
Anton Halauko <anton.halauko.main@gmail.com>
2024-02-04 16:29:47 +0200

    feat: implement eol os task

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { getUserName } from './process-arguments-handler.js';
 import { registerCommandLineHandler } from './command-line-handler/command-line-handler.js';
 import { fileHandlers } from './file-handlers/file-handlers.js';
 import { navigationHandlers } from './navigation-handlers/navigation-handlers.js';
+import { osHandlers } from './os-handlers/os-handlers.js';
 
 const handlers = [
 	...fileHandlers,
 	...navigationHandlers,
+	...osHandlers,
 ];
 
 const fileManagerState = {
---
+++ src/os-handlers/os-handlers.js
@@ -0,0 +1,5 @@
+import { eolHandler } from './eol-handler.js';
+
+export const osHandlers = [
+	eolHandler,
+];
